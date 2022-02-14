522ff83d63e4f87f0a6b4118e106e3eee9369c22
Roman <ramzos@rambler.ru>
2022-02-15 00:57:42 +0300

    DzLesson 1.7

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,5 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+// Task1
+
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich ";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сострудник - " + fullName);
+
+//Task2
+
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName);
+
+//Task3
+
+        fullName = fullName.replace(" ", ";");
+        System.out.println("Данные ФИО сотрудника для административного отдела - " + fullName);
+
+//Task4
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + fullName);
+
     }
 }
